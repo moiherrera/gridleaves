5f8e0ac18eee8daa6b32db66bc8e856b527b447d
moiherrera <moi.herrera@outlook.com>
2019-09-06 12:37:40 -0400

    Add a user_id to profile resource through AddUserToProfile Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_155750) do
+ActiveRecord::Schema.define(version: 2019_09_06_163418) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_09_06_155750) do
     t.string "community_assets"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema.define(version: 2019_09_06_155750) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "profiles", "users"
 end
