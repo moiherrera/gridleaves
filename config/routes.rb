c2f5266088089b51c8ea974766c7c95b7b5642ec
moiherrera <moi.herrera@outlook.com>
2019-09-06 12:01:26 -0400

    create all routes for profile resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :profiles
   # RESTful routes
   resources :examples, except: %i[new edit]
+  resources :profiles, except: %i[new edit]
 
   # Custom routes
   post '/sign-up' => 'users#signup'
