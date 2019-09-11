eec43af2c2427720e335cf0fc8347a5c29aa7fc3
moiherrera <moi.herrera@outlook.com>
2019-09-11 14:24:46 -0400

    Add :editable to exclusively allow user-owned resources to be viewed by user only

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,5 +1,5 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :name, :location, :image_link, :personal_assets, :professional_interests, :hobbies, :projects, :contact_info, :express, :age, :pronouns, :community_assets
+  attributes :id, :name, :location, :image_link, :personal_assets, :professional_interests, :hobbies, :projects, :contact_info, :express, :age, :pronouns, :community_assets, :editable
   belongs_to :user
 
   def editable
